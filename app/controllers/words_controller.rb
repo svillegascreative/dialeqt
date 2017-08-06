d7fa89944b84f82e64fd9c89d314bc6c6b8ef75c
sylvia villegas <sylvia@svillegas.com>
2017-08-06 13:48:20 -0400

    Create empty RCAV for words as resource

---
+++ app/controllers/words_controller.rb
@@ -0,0 +1,31 @@
+class WordsController < ApplicationController
+
+  def index
+    #code
+  end
+
+  def show
+    #code
+  end
+
+  def new
+    #code
+  end
+
+  def create
+    #code
+  end
+
+  def edit
+    #code
+  end
+
+  def update
+    #code
+  end
+
+  def destroy
+    #code
+  end
+
+end
