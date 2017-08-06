e2815f72f9297a23451ef9915a13bd5cb9d9711c
sylvia villegas <sylvia@svillegas.com>
2017-08-06 19:33:42 -0400

    Validate :username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username,
+    presence: true,
+    uniqueness: true,
+    format: { with: /^[a-zA-Z0-9](\w|\.)*[a-zA-Z0-9]$/, multiline: true, message: "Not a valid username."},
+    length: { in: 4..20 }
 end
