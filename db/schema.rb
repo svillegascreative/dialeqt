6332582c3bba0c40a48e3f2a06c1eac426c23dba
sylvia villegas <sylvia@svillegas.com>
2017-08-06 23:33:31 -0400

    Add user_id to Word

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170806205929) do
+ActiveRecord::Schema.define(version: 20170807032952) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20170806205929) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
 end
