81d01448a7e80a85a4e05a0842310ddd54f983fe
sylvia villegas <sylvia@svillegas.com>
2017-08-06 23:00:31 -0400

    Change username validation error message

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
   validates :username,
     presence: true,
     uniqueness: true,
-    format: { with: /^[a-zA-Z0-9](\w|\.)*[a-zA-Z0-9]$/, multiline: true, message: "Not a valid username."},
+    format: { with: /^[a-zA-Z0-9](\w|\.)*[a-zA-Z0-9]$/, multiline: true, message: "has an invalid character or combination of characters"},
     length: { in: 4..20 }
 end
