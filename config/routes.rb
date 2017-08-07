9bea0e4364f95f9a5e4840a8f652fc25b5dd97f4
sylvia villegas <sylvia@svillegas.com>
2017-08-06 23:50:39 -0400

    Add registrations to custom routes for Devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "words#index"
 
-  devise_for :users, controllers: { sessions: 'users/sessions'}
+  devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions'}
 
   resources :words
 
