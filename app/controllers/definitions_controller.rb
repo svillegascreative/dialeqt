e90a7b4b9e10cb8106eb171d4c70d2a5b919d2d1
sylvia villegas <sylvia@svillegas.com>
2017-08-07 00:40:21 -0400

    Fill in basic actions for Definition

--- app/controllers/definitions_controller.rb
+++ app/controllers/definitions_controller.rb
@@ -1,22 +1,54 @@
 class DefinitionsController < ApplicationController
+  before_action :authenticate_user!
 
   def new
-    #code
+    @definition = Definition.new
   end
 
   def create
-    #code
+    @definition = Definition.new(definition_params)
+    find_word
+    @definition.word_id = @word.id
+    @definition.user_id = current_user.id
+
+    if @definition.save
+      redirect_to @word
+    else
+      render :new
+    end
   end
 
   def edit
-    #code
+    find_definition
   end
 
   def update
-    #code
+    find_definition
+
+    if @definition.update_attributes(definition_params)
+      redirect_to @definition
+    else
+      render :edit
+    end
   end
 
   def destroy
-    #code
+    find_definition
+    @definition.destroy
+    redirect_to @word
+  end
+
+private
+
+  def find_definition
+    @definition = Definition.find[:id]
+  end
+
+  def find_word
+    @word = Word.find[:id]
+  end
+
+  def definition_params
+    require(:deifnition).permit(:details, :example, :user_id, :word_id)
   end
 end
