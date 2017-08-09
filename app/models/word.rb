796e54993ab95d8e7a2b35af2bc66dbe7bb66fe3
sylvia villegas <sylvia@svillegas.com>
2017-08-09 17:47:26 -0400

    Add acts_as_url to Word

--- app/models/word.rb
+++ app/models/word.rb
@@ -2,4 +2,14 @@ class Word < ApplicationRecord
   has_many :definitions
 
   acts_as_votable
+
+  acts_as_url :name,
+    url_attribute: :slug,
+    sync_url: true,
+    blacklist: %W{new edit},
+    blacklist_policy: :blacklist_policy
+
+  def blacklist_policy
+    Proc.new {|w| "word-#{w}"}
+  end
 end
