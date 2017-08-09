d3f4b1ca6b9c9ef5fa61515fd522ac54eb273697
sylvia villegas <sylvia@svillegas.com>
2017-08-08 21:59:15 -0400

    Make User act_as_voter

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
     uniqueness: true,
     format: { with: /^[a-zA-Z0-9](\w|\.)*[a-zA-Z0-9]$/, multiline: true, message: "has an invalid character or combination of characters"},
     length: { in: 4..20 }
+
+  acts_as_voter
 end
