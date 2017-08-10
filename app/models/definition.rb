ea31fd593d8cdba96c95dafd00eb292b4f7c9624
sylvia villegas <sylvia@svillegas.com>
2017-08-10 19:55:04 -0400

    Add search method to Def'n

--- app/models/definition.rb
+++ app/models/definition.rb
@@ -1,8 +1,16 @@
 class Definition < ApplicationRecord
+  # include Searchable
   belongs_to :word
   belongs_to :user
 
   validates :details, presence: true
 
   acts_as_votable
+
+  def self.search(query, columns)
+    where(
+      columns.map{|c| "#{c} ilike :search" }.join(' OR '),
+      search: "%#{query}%"
+    )
+  end
 end
