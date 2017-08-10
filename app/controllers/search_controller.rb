ca82cbfc79692410b67a6035e448e814331040c4
sylvia villegas <sylvia@svillegas.com>
2017-08-10 19:22:59 -0400

    Create search#index

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,2 +1,12 @@
 class SearchController < ApplicationController
+
+  def index
+    if params[:query]
+      @query = params[:query]
+      @words = Word.search(@query, [:name])
+    else
+      flash[:notice] = "No search query provided"
+    end
+  end
+
 end
