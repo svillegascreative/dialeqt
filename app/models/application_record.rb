2c5e4482bd087f333a0058ff9a3250ea44886e0b
sylvia villegas <sylvia@svillegas.com>
2017-08-11 22:25:47 -0400

    Create strftime methods for all models

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,6 +1,20 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
+  def when_created
+    self.created_at.strftime("%b %-d '%y")
+  end
+
+  def when_updated
+    self.updated_at.strftime("%b %-d '%y")
+  end
+
+  def when_if_updated
+    if self.updated_at != self.created_at
+      "(Edited #{self.when_updated})"
+    end
+  end
+
   # Use only on models with acts_as_votable
   def get_wilson_score
     positive = self.cached_votes_up
