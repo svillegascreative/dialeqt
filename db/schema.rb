33f28cf2e6411935dc60ad34b085a8695e763acc
sylvia villegas <sylvia@svillegas.com>
2017-08-13 14:21:13 -0400

    Create trigram migr'n for Textacular

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170810034845) do
+ActiveRecord::Schema.define(version: 20170813181507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_trgm"
 
   create_table "definitions", force: :cascade do |t|
     t.text     "details"
@@ -31,6 +32,15 @@ ActiveRecord::Schema.define(version: 20170810034845) do
     t.index ["cached_votes_score"], name: "index_definitions_on_cached_votes_score", using: :btree
     t.index ["cached_votes_total"], name: "index_definitions_on_cached_votes_total", using: :btree
     t.index ["cached_votes_up"], name: "index_definitions_on_cached_votes_up", using: :btree
+  end
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text     "content"
+    t.string   "searchable_type"
+    t.integer  "searchable_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
