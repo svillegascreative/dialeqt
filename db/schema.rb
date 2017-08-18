70e534bf6a201ce2f8ee1ad8c0ee773795dd772b
sylvia villegas <sylvia@svillegas.com>
2017-08-18 14:41:40 -0400

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170813181507) do
+ActiveRecord::Schema.define(version: 20170818182233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,27 @@ ActiveRecord::Schema.define(version: 20170813181507) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id", using: :btree
+  end
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer  "word_id"
+    t.integer  "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tag_id"], name: "index_taggings_on_tag_id", using: :btree
+    t.index ["word_id"], name: "index_taggings_on_word_id", using: :btree
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_tags_on_name", unique: true, using: :btree
+  end
+
+  create_table "trigrams", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -93,4 +114,6 @@ ActiveRecord::Schema.define(version: 20170813181507) do
     t.index ["slug"], name: "index_words_on_slug", unique: true, using: :btree
   end
 
+  add_foreign_key "taggings", "tags"
+  add_foreign_key "taggings", "words"
 end
