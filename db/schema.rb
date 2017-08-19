10903e0688b2470629546bb44cf7d6da2422af3c
sylvia villegas <sylvia@svillegas.com>
2017-08-19 15:10:03 -0400

    Add :slug column to tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170818182233) do
+ActiveRecord::Schema.define(version: 20170819185811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20170818182233) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "slug"
     t.index ["name"], name: "index_tags_on_name", unique: true, using: :btree
   end
 
