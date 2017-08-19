f7b305c2e8eeb4dcb373cf7625052eae0891b075
sylvia villegas <sylvia@svillegas.com>
2017-08-19 15:10:39 -0400

    Use pretty urls for tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,4 +3,22 @@ class Tag < ApplicationRecord
   has_many :words, through: :taggings
 
   validates_uniqueness_of :name
+
+  acts_as_url :name,
+    url_attribute: :slug,
+    sync_url: true,
+    blacklist: %W{new edit},
+    blacklist_policy: :blacklist_policy
+
+  def self.find(input)
+    find_by_slug(input)
+  end
+
+  def to_param
+    slug
+  end
+
+  def blacklist_policy
+    Proc.new {|w| "tag-#{w}"}
+  end
 end
