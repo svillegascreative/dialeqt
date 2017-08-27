52ac3fe3034116fb2cfd0c1e2424b2a3bd20d9f2
sylvia villegas <sylvia@svillegas.com>
2017-08-26 21:27:55 -0400

    Add flag_by method for flaggables

--- app/models/concerns/flaggable.rb
+++ app/models/concerns/flaggable.rb
@@ -4,4 +4,10 @@ module Flaggable
   included do
     has_many :flaggings, as: :flaggable, dependent: :destroy
   end
+
+  def flag_by(user, options = {})
+    reason = options[:reason] || nil
+    comment = options[:comment] || nil
+    flaggings.build(flagger_id: user.id, flagger_type: user.class, reason: reason, comment: comment)
+  end
 end
