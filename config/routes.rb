10e0ca5341f485639657f021a051150277981566
sylvia villegas <sylvia@svillegas.com>
2017-08-28 12:02:35 -0400

    Add flagging routes under Words
    And tidy up some other lines

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "words#index"
 
   get 'search/', to: 'search#index', as: 'search'
@@ -8,21 +7,23 @@ Rails.application.routes.draw do
 
   resources :words do
     member do
-      put "like", to: "words#like"
-      put "dislike", to: "words#dislike"
+      put 'like', to: 'words#like'
+      put 'dislike', to: 'words#dislike'
     end
     collection do
       get 'check', to: 'words#check'
     end
+    resources :flaggings, except: [:index, :show, :new]
     resources :definitions, only: [:new, :create]
   end
 
   resources :definitions, only: [:edit, :update, :destroy] do
      member do
-      put "upvote", to: "definitions#upvote"
-      put "downvote", to: "definitions#downvote"
-     end
+      put 'upvote', to: 'definitions#upvote'
+      put 'downvote', to: 'definitions#downvote'
+    end
   end
 
   resources :tags, only: [:index, :show]
+  # resources :flaggings, except: [:index, :show]
 end
