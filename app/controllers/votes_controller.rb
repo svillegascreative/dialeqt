bead1d01575c460eb891f0e13977987d0de3f675
sylvia villegas <sylvia@svillegas.com>
2017-09-08 17:27:47 -0400

    Add vote reate/destroy actions

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,28 @@
 class VotesController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @votable = get_votable
+    @votable.toggle_upvote current_user
+    @votable.update_wilson_score
+    # redirect_to @votable
+  end
+
+  def destroy
+    @votable = get_votable
+    @votable.toggle_downvote current_user
+    @votable.update_wilson_score
+    # redirect_to @votable
+  end
+
+private
+
+  def get_votable
+    if params[:word_id]
+      Word.find(params[:word_id])
+    elsif params[:definition_id]
+      Definition.find(params[:definition_id])
+    end
+  end
+
 end
