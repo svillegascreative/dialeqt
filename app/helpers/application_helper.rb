c0e0d5f4788173161be940882503e07395886e34
sylvia villegas <sylvia@svillegas.com>
2017-09-13 10:42:15 -0400

    Create helper: adds "active" class to tab

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def tab_link(link_name, link_path)
+    active_class = current_page?(link_path) ? "active" : ""
+    content_tag(:li, class: active_class) do
+      link_to link_name, link_path
+    end
+  end
+  
 end
